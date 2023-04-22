15f314de75360cf059812e4df6a3235712392eb7
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2023-04-22 18:46:44 +0530

    Refactor Code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,56 +1,53 @@
-const UserModal = require('../models/user.model');
+const AuthService = require('../services/auth.service');
 const jwtConfig = require('../config/jwt.config');
-const cache = require('../utils/cache.util');
-const jwt = require('../utils/jwt.util');
-const bcrypt = require('bcrypt');
+const bcryptUtil = require('../utils/bcrypt.util');
+const jwtUtil = require('../utils/jwt.util');
 
-exports.register = async (req, res) => {
-    const isExist = await UserModal.findOne({
-        email: req.body.email
-    })
+exports.register = async (req, res) => { 
+    const isExist = await AuthService.findUserByEmail(req.body.email);
     if(isExist) {
-        return res.status(400).json({ message: 'Email already exists.' });
+        return res.status(400).json({ 
+            message: 'Email already exists.' 
+        });
     }
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-    const user = await UserModal.create({
+    const hashedPassword = await bcryptUtil.createHash(req.body.password);
+    const userData = {
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword
+    }
+    const user = await AuthService.createUser(userData);
+    return res.json({
+        data: user,
+        message: 'User registered successfully.'
     });
-    return res.json(user);
 }
 
-exports.login = async (req, res) => {
-    const user = await UserModal.findOne({
-        email: req.body.email
-    });
+exports.login = async (req, res) => { 
+    const user = await AuthService.findUserByEmail(req.body.email); 
     if (user) {
-        const isMatched = await bcrypt.compare(req.body.password, user.password);
+        const isMatched = await bcryptUtil.compareHash(req.body.password, user.password);
         if (isMatched) {
-            const token = await jwt.createToken({ _id: user._id });
-            return res.json({ 
+            const token = await jwtUtil.createToken({ _id: user._id });
+            return res.json({
                 access_token: token,
                 token_type: 'Bearer',
                 expires_in: jwtConfig.ttl
             });
         }
     }
-    return res.status(400).json({ message: 'Invalid credentails.' });
+    return res.status(400).json({ message: 'Unauthorized.' });
 }
 
 exports.getUser = async (req, res) => {
-    const user = await UserModal.findById(req.user._id);
-    return res.json(user);
+    const user = await AuthService.findUserById(req.user._id);  
+    return res.json({
+        data: user,
+        message: 'Success.'
+    });
 }
 
-exports.logout = async (req, res) => { 
-    const token = req.token;
-    const now = new Date();
-    const expire = new Date(req.user.exp);
-    const milliseconds = now.getTime() - expire.getTime();
-    /* ----------------------------- BlackList Token ---------------------------- */
-    await cache.set(token, token, milliseconds);
-
-    return res.json({ message: 'Logged out successfully' });
+exports.logout = async (req, res) => {    
+    await AuthService.logoutUser(req.token, req.user.exp);  
+    return res.json({ message: 'Logged out successfully.' });
 }
---
+++ services/auth.service.js
@@ -0,0 +1,22 @@
+const UserModel = require('../models/user.model');
+const cacheUtil = require('../utils/cache.util');
+
+exports.createUser = (user) => {
+    return UserModel.create(user);
+}
+
+exports.findUserByEmail = (email) => {
+    return UserModel.findOne({ email: email })
+}
+
+exports.findUserById = (id) => {
+    return UserModel.findById(id);
+}
+
+exports.logoutUser = (token, exp) => {
+    const now = new Date();
+    const expire = new Date(exp * 1000);
+    const milliseconds = expire.getTime() - now.getTime();
+    /* ----------------------------- BlackList Token ---------------------------- */
+    return cacheUtil.set(token, token, milliseconds);
+}
